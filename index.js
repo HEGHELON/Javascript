879e16fd7023737cc241d927ee778b3dbc98bc3c
Eghelonohor Akpotaire Humphrey <humphreyeghelonohor@yahoo.com>
2022-09-15 13:22:57 +0000

    Tip calculator app

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log(5+5);
-const name = 5;
-console.log(name);
+// console.log(5+5);
+// const name = 5;
+// console.log(name);
+
+// Tip Calculator App
+const food = 50
+const tipPercentage = 0.2
+const tipAmount = food * tipPercentage
+const totalFoodBill = food + tipAmount
+
+console.log(totalFoodBill)
